fd5961566f9a711165f8df78a80fca69d9719866
Zafra <zafraziaudeen@gmail.com>
2025-02-24 01:45:18 +0530

    vehicle.java

--- cabservice/src/main/java/com/cabservice/model/Vehicle.java
+++ cabservice/src/main/java/com/cabservice/model/Vehicle.java
@@ -6,7 +6,7 @@ public class Vehicle {
     private String model;
     private int capacity;
     private double ratePerKm;
-    private String status; 
+    private String status;
 
     // ✅ Constructors
     public Vehicle() {}
